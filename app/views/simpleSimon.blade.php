fff8c9fa7e644584f5f07cddfce803be51a3e9d1
Dylan Perenchio <perenchiod@gmail.com>
2015-09-02 13:46:29 -0500

    .

--- app/views/simpleSimon.blade.php
+++ app/views/simpleSimon.blade.php
@@ -19,9 +19,6 @@
 	</div>
 	<button id="startGame">Start the game!</button>
 	
-
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
 <script>
 	"use strict";
 	var square0 = $("#square0");
